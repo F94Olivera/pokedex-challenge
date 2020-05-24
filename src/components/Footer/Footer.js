bac4abd83969946182c87c3d26538f929ea3f4ee
Facu Olivera <f94olivera@gmail.com>
2020-05-24 16:00:47 -0300

    footer con darkmode

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 function Footer() {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
   return (
-    <div className="d-flex justify-content-center">
-      <p>
+    <div
+      className="d-flex justify-content-center"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <p style={{ background: theme.ui }}>
         {'<> '}
-        with <span className="icon">&#9829;</span> by F94
+        with{' '}
+        <span className="icon" style={{ color: theme.syntax }}>
+          &#9829;
+        </span>{' '}
+        by F94
       </p>
     </div>
   );
