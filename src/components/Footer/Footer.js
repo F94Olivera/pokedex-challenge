88430fc7793f261f479c2680a79638c08b79f25d
Facu Olivera <f94olivera@gmail.com>
2020-05-24 14:41:15 -0300

    rm inline styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,7 @@ function Footer() {
     <div className="d-flex justify-content-center">
       <p>
         {'<> '}
-        with{' '}
-        <span className="icon" style={{ color: '#000000' }}>
-          &#9829;
-        </span>{' '}
-        by F94
+        with <span className="icon">&#9829;</span> by F94
       </p>
     </div>
   );
