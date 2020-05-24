e0f5c42edf946151596df91edf55abba649b42f1
F94 <f94olivera@gmail.com>
2020-05-24 12:40:04 -0300

    feature multilanguage

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,38 +4,37 @@ import axios from 'axios';
 const POKE_API = 'https://pokeapi.co/api/v2/pokemon-species/';
 
 function Modal({ modalCard }) {
-  // const [loading, setLoading] = useState(false);
   const [pokemon, setPokemon] = useState(modalCard);
   const [isEn, setIsEn] = useState(true);
-  // console.log('MODAL COMPONENT modalCard ', modalCard);
 
   useEffect(() => {
     const fetchCard = async () => {
-      // setLoading(true);
       const res = await axios.get(
         `${POKE_API}${modalCard.name ? modalCard.name : 1}`
       );
 
       setPokemon(res.data);
-      // setLoading(false);
     };
 
     fetchCard();
-  }, []);
+  }, [modalCard]);
 
   const parse = (type) => {
-    console.log('pokemon ', pokemon);
-
+    let data;
     if (
       type === 'flavor_text' &&
       pokemon.hasOwnProperty('flavor_text_entries')
     ) {
-      let data;
-      // if(isEn)
-      data = pokemon.flavor_text_entries.find(
-        (item) => item.language.name === 'en'
-      );
-      console.log('data ', data);
+      if(isEn){
+        data = pokemon.flavor_text_entries.find(
+          (item) => item.language.name === 'en'
+        );
+      }else{
+        data = pokemon.flavor_text_entries.find(
+          (item) => item.language.name === 'es'
+        );
+      }
+      return data.flavor_text;
     }
   };
 
@@ -68,7 +67,6 @@ function Modal({ modalCard }) {
                 ES
               </button>
             </div>
-
             <button
               type="button"
               className="close"
@@ -83,11 +81,6 @@ function Modal({ modalCard }) {
               <img src="4.png" className="modal-img card-img-top" alt="..." />
             </div>
             <h5 className="text-center mt-2">{modalCard.name}</h5>
-            <p className="text-center">
-              Bulbasaur can be seen napping in bright sunlight.\nThere is a seed
-              on its back. By soaking up the sun’s rays,\nthe seed grows
-              progressively larger.
-            </p>
             <p className="text-center">{parse('flavor_text')}</p>
           </div>
         </div>
