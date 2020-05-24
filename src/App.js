39a68c8f04b829a45ab4ef4731e2ca7dc6f72b9b
Facu Olivera <f94olivera@gmail.com>
2020-05-24 16:36:29 -0300

    added ThemeProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+import ThemeContextProvider from './contexts/ThemeContext';
 import Navbar from './components/Navbar/Navbar';
 import Modal from './components/Modal/Modal';
 import Cards from './components/Cards/Cards';
@@ -46,25 +47,27 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      <Modal
-        modalCard={modalCard}
-        POKE_API_SPECIE={POKE_API_SPECIE}
-        POKE_API_SPRITE={POKE_API_SPRITE}
-      />
-      <Cards
-        cards={currentCards}
-        loading={loading}
-        indexOfFirstCard={indexOfFirstCard}
-        modalDetails={modalDetails}
-        POKE_API_SPRITE={POKE_API_SPRITE}
-      />
-      <Pagination
-        cardsPerPage={cardsPerPage}
-        totalCards={cards.length}
-        paginate={paginate}
-      />
-      <Footer />
+      <ThemeContextProvider>
+        <Navbar />
+        <Modal
+          modalCard={modalCard}
+          POKE_API_SPECIE={POKE_API_SPECIE}
+          POKE_API_SPRITE={POKE_API_SPRITE}
+        />
+        <Cards
+          cards={currentCards}
+          loading={loading}
+          indexOfFirstCard={indexOfFirstCard}
+          modalDetails={modalDetails}
+          POKE_API_SPRITE={POKE_API_SPRITE}
+        />
+        <Pagination
+          cardsPerPage={cardsPerPage}
+          totalCards={cards.length}
+          paginate={paginate}
+        />
+        <Footer />
+      </ThemeContextProvider>
     </div>
   );
 }
