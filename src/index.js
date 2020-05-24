aa6f7eb9579b7c26bd2412fd129c7d0da5681a26
Facu Olivera <f94olivera@gmail.com>
2020-05-24 16:36:04 -0300

    rm library

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import 'dark-mode-switch/dark-mode.css';
-// import 'dark-mode-switch/dark-mode-switch.min';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
