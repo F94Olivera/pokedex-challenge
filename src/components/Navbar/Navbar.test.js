02a0f2e1fdebb83cc82ab8203e88f70efc61967e
Facu Olivera <f94olivera@gmail.com>
2020-05-25 12:31:58 -0300

    Navbar test

---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Navbar from './Navbar';
+import ThemeContextProvider from '../../contexts/ThemeContext';
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+describe('<Navbar />', () => {
+  it('>>>> should render all elements', () => {
+    // const { isLightTheme } = React.useContext(ThemeContext);
+    const wrapper = mount(
+      <ThemeContextProvider>
+        <Navbar />
+      </ThemeContextProvider>
+    );
+
+    expect(wrapper.find('div.container-fluid')).toBeDefined();
+    expect(wrapper.find('nav.navbar')).toHaveLength(1);
+    expect(wrapper.find('h5.navbar-brand')).toHaveLength(1);
+    expect(wrapper.find('div.custom-control')).toHaveLength(1);
+  });
+});
