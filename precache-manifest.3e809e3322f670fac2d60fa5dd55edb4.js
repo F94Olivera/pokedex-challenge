94565407994cf2a2f4c9ecb5dd686d3b35881faf
Facu Olivera <f94olivera@gmail.com>
2020-05-25 12:48:57 -0300

    Updates

---
+++ precache-manifest.3e809e3322f670fac2d60fa5dd55edb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1e958a6aa4f47259c1952b02d0ef97",
+    "url": "/pokedex-challenge/index.html"
+  },
+  {
+    "revision": "aef77bfdbe5a834a437f",
+    "url": "/pokedex-challenge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "34f7e97ba36dd4985e10",
+    "url": "/pokedex-challenge/static/css/main.6e832683.chunk.css"
+  },
+  {
+    "revision": "aef77bfdbe5a834a437f",
+    "url": "/pokedex-challenge/static/js/2.5fe72640.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/pokedex-challenge/static/js/2.5fe72640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f7e97ba36dd4985e10",
+    "url": "/pokedex-challenge/static/js/main.978008a3.chunk.js"
+  },
+  {
+    "revision": "458dbf99bc20f6e441af",
+    "url": "/pokedex-challenge/static/js/runtime-main.544fe7f2.js"
+  }
+]);
